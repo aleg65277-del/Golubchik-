945c9ea5edfe5ecedab4d0c83fb043a1f950e5d9
aleg65277-del <aleg65277@gmail.com>
2026-02-12 20:39:02 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, { cors: { origin: "*" } });
+
+app.use(express.static("public"));
+
+io.on("connection", socket => {
+  console.log("User connected");
+
+  socket.on("join", room => {
+    socket.join(room);
+  });
+
+  socket.on("message", data => {
+    io.to(data.room).emit("message", data);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log("Golubchik running on", PORT));
