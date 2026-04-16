555a990a7f2e33b86fa8742432b22f8fc7b59e14
aleg65277-del <aleg65277@gmail.com>
2026-04-17 00:27:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, { cors: { origin: "*" } });
+const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
-io.on("connection", socket => {
-  console.log("User connected");
+let users = {};
 
-  socket.on("join", room => {
-    socket.join(room);
+io.on("connection", socket => {
+
+  socket.on("join", data => {
+    socket.room = data.room;
+    socket.name = data.user;
+
+    socket.join(data.room);
+
+    if(!users[data.room]) users[data.room] = [];
+    users[data.room].push(data.user);
+
+    io.to(data.room).emit("users", users[data.room]);
   });
 
   socket.on("message", data => {
     io.to(data.room).emit("message", data);
   });
+
+  socket.on("disconnect", () => {
+    if(socket.room && users[socket.room]){
+      users[socket.room] = users[socket.room].filter(u => u !== socket.name);
+      io.to(socket.room).emit("users", users[socket.room]);
+    }
+  });
+
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log("Golubchik running on", PORT));
+http.listen(PORT, () => console.log("Golubchik PRO running"));
